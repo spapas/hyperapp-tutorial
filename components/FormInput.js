8f468e96a80f5a134f38c02bfa8346afc4457eb5
Serafeim Papastefanos <spapas@gmail.com>
2018-03-22 13:28:36 +0200

    Small fix

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,8 +1,8 @@
 const { h } = require('hyperapp')
 
-const FormInput = module.exports = ({label, value, action, type='text'}) => <div class="form-group">
+const FormInput = module.exports = ({label, key, value, action, type='text'}) => <div class="form-group" key={key}>
     <label class="form-label" for="{label}">{label}</label>
-    <input class="form-input" type={type} id={label}
+    <input class="form-input" type={type} id={key}
         placeholder={label} value={value} 
         oninput={e => action(e.target.value)}
     />
