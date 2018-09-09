3eb48753fd6426a98df7053e72af4be4117e94bc
Serafeim Papastefanos <spapas@gmail.com>
2018-09-09 22:40:12 +0300

    Eslint

--- components/ModalForm.js
+++ components/ModalForm.js
@@ -2,39 +2,38 @@ import { h } from 'hyperapp'
 import { FormInput, FormInputLong} from './FormInputs.js'
 import { SpinnerSmall } from '../components/Spinners.js'
 
-
 const renderField = (field, updateFieldAction ) => {
-    let ftype = FormInput 
-    if(field.type == 'longtext') ftype = FormInputLong
-    return ftype({
-        field,
-        action: (val) => updateFieldAction(field.key, val)
-    });
+  let ftype = FormInput
+  if(field.type == 'longtext') ftype = FormInputLong
+  return ftype({
+    field,
+    action: (val) => updateFieldAction(field.key, val)
+  });
 }
 
 const renderFields = (fields,  updateFieldAction) => fields.map(
-    f => renderField(f, updateFieldAction)
+  f => renderField(f, updateFieldAction)
 )
 
 const ModalForm = module.exports = ({ loading, formFields, item, hideAction, saveAction, updateFieldAction }) => <div className={`modal ${item?'active':''}`}>
-    <div class="modal-overlay"></div>
-    <div class="modal-container">
-        <div class="modal-header">
-            <button class="btn btn-clear float-right" onclick={hideAction}></button>
-            <div class="modal-title h5">{item.id?`Editing item ${item.id}`:"Add new item!"}</div>
-        </div>
-        <div class="modal-body">
-            <div class="content">
-                <form method='POST'>
-                    {renderFields(formFields, updateFieldAction)}
-                </form>
-            </div>
-        </div>
-        <div class="modal-footer">
-            {loading?<SpinnerSmall />:<div>
-                <button class="btn" onclick={hideAction}>Cancel</button>
-                <button class="ml-2 btn btn-primary" onclick={saveAction}>Ok</button>
-            </div>}
-        </div>
+  <div class="modal-overlay"></div>
+  <div class="modal-container">
+    <div class="modal-header">
+      <button class="btn btn-clear float-right" onclick={hideAction}></button>
+      <div class="modal-title h5">{item.id?`Editing form for item ${item.id}`:"Add new item!"}</div>
     </div>
+    <div class="modal-body">
+      <div class="content">
+        <form method='POST'>
+          {renderFields(formFields, updateFieldAction)}
+        </form>
+      </div>
+    </div>
+    <div class="modal-footer">
+      {loading?<SpinnerSmall />:<div>
+        <button class="btn" onclick={hideAction}>Cancel</button>
+        <button class="ml-2 btn btn-primary" onclick={saveAction}>Ok</button>
+      </div>}
+    </div>
+  </div>
 </div>
